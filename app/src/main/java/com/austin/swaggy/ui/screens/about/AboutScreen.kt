5b98b8925869eab3d713d271e8025be5d106a4d0
AUSTINMWANIKI <austinmwaniki112@gmail.com>
2025-07-24 05:06:38 -0400

    final commit

---
+++ app/src/main/java/com/austin/swaggy/ui/screens/about/AboutScreen.kt
@@ -0,0 +1,109 @@
+package com.austin.swaggy.ui.screens.about
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.austin.swaggy.R
+import com.austin.swaggy.ui.theme.Pink40
+
+@Composable
+fun AboutScreen(){
+
+    Column (
+        modifier=Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+
+    Image(
+        painter = painterResource(R.drawable.visualdisplayunit),
+        contentDescription = "marketable",
+        modifier = Modifier.size(300.dp),
+    )
+
+    Text(
+        text = "Easy Shopping",
+        color = Color.Blue,
+        fontSize = 40.sp,
+        fontWeight = FontWeight.Bold,
+    )
+
+    Text(
+        text = "Children, Women, Clothes, Wedding, Dress, Latest Products, we always have a new products, New Stayl2025 Girl, New Stayl2025 Girl, Show Details.",
+        textAlign = TextAlign.Center,
+        fontSize = 20.sp,
+
+    )
+    Spacer(modifier = Modifier.height(180.dp))
+
+    Row (){
+        Box(modifier = Modifier.size(10.dp).background(color = Color.Blue, shape = CircleShape),
+
+        )
+        Spacer(modifier = Modifier.height(100.dp))
+
+    Row (){
+        Box(modifier = Modifier.size(10.dp).background(color = Color.Blue, shape = CircleShape),
+
+            )
+        Spacer(modifier = Modifier.height(100.dp))
+
+    Row (){
+        Box(modifier = Modifier.size(10.dp).background(color = Color.Blue, shape = CircleShape),
+            )
+        Spacer(modifier = Modifier.height(100.dp))
+
+    }
+    }
+
+
+
+
+        Text(
+            text = "Skip",
+            color = Color.Blue,
+            textAlign = TextAlign.Right,
+            fontSize = 25.sp,
+            modifier = Modifier.height(100.dp).padding(start = 200.dp, bottom = 25.dp),
+        )
+
+    }
+
+
+
+    }
+
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AboutScreenPreview(){
+    AboutScreen()
+}
---
+++ app/src/main/java/com/austin/swaggy/ui/screens/home/HomeScreen.kt
@@ -0,0 +1,96 @@
+package com.austin.swaggy.ui.screens.home
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.austin.swaggy.R
+import com.austin.swaggy.ui.theme.Pink40
+import com.austin.swaggy.ui.theme.newIndigo
+
+@Composable
+fun HomeScreen(){
+
+    Column (
+        modifier = Modifier.fillMaxSize().background(newIndigo),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+
+
+        Image(
+            painter = painterResource(R.drawable.chandrasekar),
+            contentDescription = "limit",
+            modifier=Modifier.size(300.dp).clip(shape = CircleShape),
+            contentScale = ContentScale.Crop,
+        )
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        Text(
+            text = "Swaggy",
+            fontSize = 40.sp,
+            color = newIndigo,
+            fontFamily = FontFamily.Cursive,
+            fontWeight = FontWeight.Bold,
+        )
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        Text(
+            text = "Get better supply!!!",
+            fontSize = 15.sp,
+            fontWeight = FontWeight.Bold
+        )
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        Text(
+            text = "A model of electrically conducting fluids that treats all interpenetrating particle species together as a single continuous medium!!!",
+            fontSize = 15.sp,
+            textAlign = TextAlign.Center,
+        )
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        Button(onClick = {},
+            colors = ButtonDefaults.buttonColors(Pink40),
+            shape = RoundedCornerShape(10.dp),
+            modifier = Modifier.fillMaxWidth().padding(start = 20.dp, end = 20.dp),
+
+        ) {
+            Text(text = "Get started")
+        }
+
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomeScreenPreview(){
+    HomeScreen()
+}
